Bug fix: beta prior calculation

methods/lenet5_switch_integral.py
@@ -48,7 +48,7 @@ layer="c1"
 how_many_epochs=200
 annealing_steps = float(8000. * how_many_epochs)
 beta_func = lambda s: min(s, annealing_steps) / annealing_steps
-alpha_0 = 1  # below 1 so that we encourage sparsity
+alpha_0_param = 1  # below 1 so that we encourage sparsity
 
 # hidden_dim = 10 #it's a number of parameters we want to estimate, e.g. # conv1 filters
 hidden_dims={'c1': conv1, 'c3': conv2, 'c5': fc1, 'f6' : fc2}
@@ -390,13 +390,12 @@ def loss_functionKL(prediction, true_y, S, alpha_0, hidden_dim, how_many_samps, 
 
 # KL-divergence between two Generalized Dirichlet distributions. Cannot do D * alpha_0 / beta_0 as sum includes other terms.
 # Instead create a tensor filled with alpha_0 / beta_0 value of size phi_alpha / phi_beta
-def loss_functionKL_GD(prediction, true_y, phi_alpha, phi_beta, alpha_0, beta_0, how_many_samps, annealing_rate):
+def loss_functionKL_GD(prediction, true_y, phi_alpha, phi_beta, alpha_0_param, how_many_samps, annealing_rate):
     CE = criterion(prediction, true_y)
 
-    alpha_0 = torch.full_like(phi_alpha, alpha_0, dtype=torch.float32).to(device)
+    alpha_0 = torch.full_like(phi_alpha, alpha_0_param, dtype=torch.float32).to(device)
     n_dim = phi_alpha.shape[0]
-    beta = torch.tensor([(n_dim-i) * 1 for i in range(n_dim)], dtype=torch.float32).to(device)
-    beta_0 = torch.full_like(phi_beta, beta_0).to(device)
+    beta_0 = torch.tensor([(n_dim-i) * alpha_0_param for i in range(n_dim)], dtype=torch.float32).to(device)
     firstTerm = torch.sum(torch.lgamma(phi_alpha + phi_beta) - torch.lgamma(phi_alpha) - torch.lgamma(phi_beta))
     secondTerm =  torch.sum(torch.lgamma(alpha_0 + beta_0) - torch.lgamma(alpha_0) - torch.lgamma(beta_0))
     thirdTerm = torch.sum((phi_alpha - alpha_0) * (torch.digamma(phi_alpha) - torch.digamma(phi_alpha + phi_beta)) +
@@ -488,7 +487,7 @@ def run_experiment(epochs_num, layer, nodesNum1, nodesNum2, nodesFc1, nodesFc2, 
             #loss=criterion(outputs, labels)
             hidden_dim = hidden_dims[layer]
             # loss = loss_functionKL(outputs, labels, S, alpha_0, hidden_dim, BATCH_SIZE, annealing_rate)
-            loss = loss_functionKL_GD(outputs, labels, phi_alpha, phi_beta, alpha_0, beta_0, BATCH_SIZE, annealing_rate)
+            loss = loss_functionKL_GD(outputs, labels, phi_alpha, phi_beta, alpha_0_param, BATCH_SIZE, annealing_rate)
             #loss=loss_function(outputs, labels, 1, 1, 1, 1)
             loss.backward()
             print("phi_alpha: ", phi_alpha)